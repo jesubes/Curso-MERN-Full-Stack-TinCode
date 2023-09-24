3eaad4f72f2a55adce2707eb735870d3e96dbc18
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-09-24 14:59:41 -0300

    C 138 - Controlando la imagen - con useDropzone

--- src/components/Admin/Courses/CourseForm/CourseForm.js
+++ src/components/Admin/Courses/CourseForm/CourseForm.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Form, Image } from "semantic-ui-react";
+import {useDropzone} from 'react-dropzone'
 import "./CourseForm.scss";
 
+
 export const CourseForm = () => {
+  const onDrop =  useCallback((acceptedFiles)=>{
+    const file = acceptedFiles[0];
+    console.log(file);
+  })
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/jpeg, image/png',
+    onDrop,
+  })
   const getMiniature = () => {
     return null;
   }
 
   return (
     <Form className="course-form">
-      <div className="course-form__miniature">
+      <div className="course-form__miniature" {...getRootProps()}>
+        <input {...getInputProps()}/>
         {getMiniature() ? (
           <Image size="small" src={ getMiniature() }/>
         ) : (
