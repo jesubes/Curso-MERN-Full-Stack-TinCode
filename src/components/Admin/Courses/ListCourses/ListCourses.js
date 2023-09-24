57e9da937da24aba3deadde4ae55fcdbc6b5700f
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-09-24 08:24:49 -0300

    Paginacion seccion Cursos - completo

--- src/components/Admin/Courses/ListCourses/ListCourses.js
+++ src/components/Admin/Courses/ListCourses/ListCourses.js
@@ -1,27 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import { Course } from '../../../../api'
+import { size, map } from 'lodash'
+import { CourseItem } from '../CourseItem'
+import { Loader, Pagination } from 'semantic-ui-react'
+import './ListCourses.scss'
 
 const coursesController = new Course();
 
 export const ListCourses = () => {
   const [courses, setCourses] = useState(false)
+  const [page, setPage] = useState(1)
+  const [pagination, setPagination] = useState()
 
   useEffect(() => {
     ( async () =>{
       try{
-        const response = await coursesController.getCourses();
-        setCourses(response.payload.docs)
-        console.log('course -->',courses);
+        const response = await coursesController.getCourses({page}); //se puede poner limit: 5
+        setCourses(response.payload.docs);
+        setPagination({
+          limit: response.payload.limit,
+          page: response.payload.page,
+          pages: response.payload.totalPages,
+          total: response.payload.totalDocs,
+        })
+
       } catch (error){
         console.error( error )
       }
     })()
-  }, [])
+  }, [page])
   
+  const changePage = (_, data ) => {
+    setPage( data.activePage )
+  }
+
+  if(!courses ) return <Loader active inline="centered" />
+  if(size(courses) === 0) return 'No hay ningun curso';
+   
 
   return (
-    <>
-      <h1>LIST COURSES DESDE COMPONENTES</h1>
-    </>
+    <div className='list-courses'>
+      {map(courses, (course) =>(
+        <CourseItem key={course._id} course={course} />
+      ))}
+
+      <div className='list-courses__pagination'> 
+        <Pagination
+          totalPages={pagination.pages}
+          defaultActivePage={pagination.page}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          onPageChange={changePage}
+        />
+      </div>
+    </div>
   )
 }
--- src/pages/admin/Courses/Courses.js
+++ src/pages/admin/Courses/Courses.js
@@ -1,42 +1,41 @@
-import React, { useState } from 'react'
-import {Tab, Button} from 'semantic-ui-react'
-import { BasicModal} from '../../../components/Shared'
-import { ListCourses } from '../../../components/Admin/Courses'
-import './Courses.scss'
+import React, { useState } from "react";
+import { Tab, Button } from "semantic-ui-react";
+import { BasicModal } from "../../../components/Shared";
+import { ListCourses } from "../../../components/Admin/Courses";
+import "./Courses.scss";
 
 export const Courses = () => {
   //modal para el nuevo curso
   const [showModal, setShowModal] = useState(false);
-  
-  const onOpenCloseModal = () => setShowModal( (prevState) => !prevState )
+
+  const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
 
   const panes = [
     {
-      menuItem: 'Listado de Cursos',
-      render: () =>(
-        <Tab.Pane attached = {false} >
+      menuItem: "Listado de Cursos",
+      render: () => (
+        <Tab.Pane attached={false}>
           <ListCourses />
         </Tab.Pane>
-      )
+      ),
     },
-  ]
-
+  ];
 
   return (
     <>
-      <div className='courses-page'>
-        <div className='courses-page__add'>
-          <Button primary onClick={ onOpenCloseModal }>
+      <div className="courses-page">
+        <div className="courses-page__add">
+          <Button primary onClick={onOpenCloseModal}>
             Nuevo Curso
           </Button>
         </div>
+
+        <Tab menu={{ secondary: true }} panes={panes} />
       </div>
 
-      <Tab menu= {{secondary: true}} panes={ panes } />
-
-      <BasicModal show= {showModal} close={onOpenCloseModal} title= 'Crear Curso'>
+      <BasicModal show={showModal} close={onOpenCloseModal} title="Crear Curso">
         <p>Formulario Para crear un curso</p>
       </BasicModal>
     </>
-  )
-}
+  );
+};
