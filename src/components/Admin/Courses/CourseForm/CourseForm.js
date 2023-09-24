a2a2188ef9d768716e97c71d7654ed1044f8e7fa
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-09-24 15:56:25 -0300

    C 139 controlando y validando el formulario de curso con Formik

--- src/components/Admin/Courses/CourseForm/CourseForm.js
+++ src/components/Admin/Courses/CourseForm/CourseForm.js
@@ -1,29 +1,54 @@
 import React, { useCallback } from "react";
 import { Form, Image } from "semantic-ui-react";
-import {useDropzone} from 'react-dropzone'
+import { useDropzone } from "react-dropzone";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./CourseForm.form.js";
 import "./CourseForm.scss";
 
-
 export const CourseForm = () => {
-  const onDrop =  useCallback((acceptedFiles)=>{
+  //inicializar formik
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValues) => {
+      try {
+        console.log(formValues);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  //
+  const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
-    console.log(file);
-  })
+
+    formik.setFieldValue("miniature", URL.createObjectURL(file)); // lo mustra provisorio en el formulario
+    formik.setFieldValue("file", file);
+  });
 
   const { getRootProps, getInputProps } = useDropzone({
-    accept: 'image/jpeg, image/png',
+    accept: "image/jpeg, image/png",
     onDrop,
-  })
-  const getMiniature = () => {
-    return null;
-  }
+  });
 
+  //TRAE miniatura del curso
+  const getMiniature = () => {
+    if( formik.values.file ){
+      return formik.values.miniature;
+    }
+    return null;
+  };
+
+  
+  //
   return (
-    <Form className="course-form">
+    <Form className="course-form" onSubmit={formik.handleSubmit}>
       <div className="course-form__miniature" {...getRootProps()}>
-        <input {...getInputProps()}/>
+        <input {...getInputProps()} />
         {getMiniature() ? (
-          <Image size="small" src={ getMiniature() }/>
+          <Image size="small" src={getMiniature()} />
         ) : (
           <div>
             <span>Arrastra tu miniatura...</span>
@@ -31,24 +56,49 @@ export const CourseForm = () => {
         )}
       </div>
 
-      <Form.Input name="title" placeholder="Nombre del curso" />
-      <Form.Input name="url" placeholder="Link del curso" />
+      <Form.Input
+        name="title"
+        placeholder="Nombre del curso"
+        onChange={formik.handleChange}
+        value={formik.values.title}
+        error={formik.errors.title}
+      />
+      <Form.Input
+        name="url"
+        placeholder="Link del curso"
+        onChange={formik.handleChange}
+        value={formik.values.url}
+        error={formik.errors.url}
+      />
       <Form.TextArea
         name="description"
         placeholder="Pequeña descripción del curso"
+        onChange={formik.handleChange}
+        value={formik.values.description}
+        error={formik.errors.description}
       />
 
       <Form.Group widths="equal">
-        <Form.Input type="number" name="price" placeholder="Precio del curso" />
+        <Form.Input
+          type="number"
+          name="price"
+          placeholder="Precio del curso"
+          onChange={formik.handleChange}
+          value={formik.values.price}
+          error={formik.errors.price}
+        />
         <Form.Input
           type="number"
           name="score"
           placeholder="Puntuación del curso"
+          onChange={formik.handleChange}
+          value={formik.values.score}
+          error={formik.errors.score}
         />
       </Form.Group>
 
-      <Form.Button type="submit" primary fluid>
-        Crear curso 
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
+        Crear curso
       </Form.Button>
     </Form>
   );
