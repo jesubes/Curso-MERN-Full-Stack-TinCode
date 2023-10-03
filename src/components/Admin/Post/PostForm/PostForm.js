f08995962a9b63d5c15f8f3bc927a0aa0801ce67
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-10-03 06:42:43 -0300

    C 153 Añadiendo Editor TinyMCE para crear el Post

--- src/components/Admin/Post/PostForm/PostForm.js
+++ src/components/Admin/Post/PostForm/PostForm.js
@@ -1,49 +1,61 @@
-import React, {useCallback} from "react";
+import React, { useCallback } from "react";
 import { Form, Image } from "semantic-ui-react";
-import {useDropzone} from 'react-dropzone'
+import { useDropzone } from "react-dropzone";
+import { Editor } from "@tinymce/tinymce-react";
 import "./PostForm.scss";
 
 export const PostForm = () => {
-  const onDrop = useCallback((acceptedFile) =>{
-    const file = acceptedFile[0]
+  const onDrop = useCallback((acceptedFile) => {
+    const file = acceptedFile[0];
     console.log(file);
-  })
+  });
 
-  const { getRootProps, getInputProps } =useDropzone({
-    accept:'image/jpeg, image/png',
-    onDrop
-  })
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    onDrop,
+  });
 
   const getMiniature = () => {
-    return null
-  }
+    return null;
+  };
 
   return (
     <Form className="post-form">
-      <Form.Group widths='equal'>
-        <Form.Input name='title' placeholder='Titulo del post' />
-        <Form.Input name='path' placeholder='Path del post' />
+      <Form.Group widths="equal">
+        <Form.Input name="title" placeholder="Titulo del post" />
+        <Form.Input name="path" placeholder="Path del post" />
       </Form.Group>
 
-      {/* Editor del post */}
-
+      {/* Editor del post  TINYMCE*/}
+      <Editor
+        init={{
+          height: 400,
+          menubar: true,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | help",
+        }}
+      />
       {/* MINIATURE */}
       <div className="post-form__miniature" {...getRootProps()}>
-        <input {...getInputProps()}/>
+        <input {...getInputProps()} />
         {getMiniature() ? (
           <Image size="small" src={getMiniature()} />
         ) : (
           <div>
             <span>Arrastra tu imagen</span>
-
-            {/*  ------- AQUI  */}
-
           </div>
         )}
       </div>
 
       <Form.Button type="submit" primary fluid>
-        Crear post 
+        Crear post
       </Form.Button>
     </Form>
   );
