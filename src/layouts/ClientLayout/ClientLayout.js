df40cdfd2ca6c5f4eddc6591ea0b217d323173db
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-10-09 06:30:52 -0300

    C 158 estructura de Layout Cliente

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container } from "semantic-ui-react";
+import "./ClientLayout.scss";
 
-export const ClientLayout = ( props ) => {
+export const ClientLayout = (props) => {
   const { children } = props;
 
   return (
-    <div>
-      <h1>Estamos en ClientLayout</h1>
+    <div className="client-layout">
+      <div className="client-layout__header">
+        <p>HEADER</p>
+      </div>
 
-      { children }
+      {children}
+
+      <div className="client-layout__footer">
+        <Container>
+          <span>INFO</span>
+          <span>MENU</span>
+          <span>NEWSLETTER</span>
+        </Container>
+        <Container>
+          <span>ALL RIGHTS RESERVED</span>
+          <span>JESUS BENJAMIN ESCACENA | SR FULLSTACK DEVELOPER</span>
+        </Container>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/web/Home/Home.js
+++ src/pages/web/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-      <h1> Estamos en la HOME  </h1>
+      <h1> Estamos en la HOME - JESUS ESCACENA  </h1>
     </div>
   )
 }
