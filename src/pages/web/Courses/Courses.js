39e06153162ab50b0dcc4e7869f19bb757b6c54f
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-10-14 18:23:55 -0300

    C 168 - Estructura de la pagina de cursos

--- src/pages/web/Courses/Courses.js
+++ src/pages/web/Courses/Courses.js
@@ -1,27 +1,45 @@
-import React, {useState, useEffect} from 'react'
-import {Course} from '../../../api'
-import './Courses.scss';
+import React, { useState, useEffect } from "react";
+import { Course } from "../../../api";
+import { Container, Image, Button } from "semantic-ui-react";
+import { map } from "lodash";
+import { image } from "../../../assets";
+import "./Courses.scss";
 
-const courseController = new Course()
+const courseController = new Course();
 
 export const Courses = () => {
-  const [courses, setCourses] = useState(null)
+  const [courses, setCourses] = useState(null);
 
   useEffect(() => {
-    (
-      async() =>{
-        try{
-          const response = await courseController.getCourses();
-          setCourses(response.payload.docs);
-        } catch(error){
-          console.error( error )
-        }
+    (async () => {
+      try {
+        const response = await courseController.getCourses();
+        setCourses(response.payload.docs);
+      } catch (error) {
+        console.error(error);
       }
-    )()
-  }, [])
+    })();
+  }, []);
   return (
-    <div>
-      <h1>Estamos en Cliente COURSES </h1>
-    </div>
-  )
-}
+    <Container className="courses-page">
+      <Image src={image.academyLogo} />
+      <h2>
+        En la web vas a encontrar los mejores cursos online de programación en
+        Español. Unete a nosotros y empieza tu camino como programador frontend
+        o backend!
+      </h2>
+
+      <div className="courses">
+        {map(courses, (course) => (
+          <div key={course._id} className="courses__item">
+            <span>{course.title}</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="more">
+        <Button primary>Cargar mas...</Button>
+      </div>
+    </Container>
+  );
+};
