ab198c2fd1b33c98a849885ca6e7bf865ac31a36
Jesus Benjamin Escacena <jesubes@gmail.com>
2023-10-16 18:50:19 -0300

    C 172 Estructura del listado de post

--- src/components/Web/Blog/ListPosts/ListPosts.js
+++ src/components/Web/Blog/ListPosts/ListPosts.js
@@ -1,27 +1,51 @@
-import React, {useState, useEffect} from 'react'
-import {Post} from '../../../../api'
-import './ListPosts.scss'
+import React, { useState, useEffect } from "react";
+import { Post } from "../../../../api";
+import { Loader, Pagination } from "semantic-ui-react";
+import { map } from "lodash";
+import "./ListPosts.scss";
 
-const postController = new Post()
+const postController = new Post();
 
 export const ListPosts = () => {
-  const [posts, setPosts] = useState(null)
+  const [posts, setPosts] = useState(null);
 
   console.log(posts);
 
   useEffect(() => {
-    (async () =>{
-      try{
-        const response = await postController.getPosts(2,2);
+    (async () => {
+      try {
+        const response = await postController.getPosts(2, 2);
         setPosts(response.postPayload.docs);
-      } catch( error ){
-        console.error( error)
+      } catch (error) {
+        console.error(error);
       }
-    })()
-  }, [])
-  
+    })();
+  }, []);
+
+
+  if(!posts) return <Loader active inline='centered' />
 
   return (
-    <div>ListPosts</div>
-  )
-}
+    <div className="list-posts-web">
+      <div className="list">
+        {map(posts, (post) => (
+          <div key={post._id} className="item">
+            <span>{post.title}</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="pagination">
+        <Pagination 
+          totalPages={10}
+          defaultActivePage={1}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          secondary
+          pointing
+        />
+      </div>
+    </div>
+  );
+};
